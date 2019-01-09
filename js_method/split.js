fe3a1de01b13805c0fd6ce6a02f1bbf32c8fd245
Taeseob,Kim <35190889+taetiseob@users.noreply.github.com>
2019-01-09 19:04:00 +0900

    real_element

---
+++ js_method/split.js
@@ -0,0 +1,7 @@
+
+      var jbAry = [ 'Lorem', 'Ipsum', 'Dolor' ];
+      var jbStr1 = jbAry.join();
+      var jbStr2 = jbAry.join( ' / ' );
+      document.write( '<p>' + jbStr1 + '</p>' );
+      document.write( '<p>' + jbStr2 + '</p>' );
+ 
